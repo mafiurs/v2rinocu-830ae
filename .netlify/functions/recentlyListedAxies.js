752ea22bf48912301a16db81f23fcf02987be6b2
Marcos Blazquez <ingblazquez@gmail.com>
2022-02-20 01:31:01 -0300

    schedule function test

--- .netlify/functions/recentlyListedAxies.js
+++ .netlify/functions/recentlyListedAxies.js
@@ -9,59 +9,60 @@ const handler = async (event, context, callback) => {
         'content-type': 'application/json'
       },
       body: JSON.stringify({
-        operationName: 'GetRecentlyItemList',
+        operationName: 'GetAxieBriefList',
         variables: {
           from: 0,
-          size: 10,
-          sort: 'Latest'
+          size: 100,
+          sort: 'Latest',
+          auctionType: 'Sale',
+          criteria: {}
         },
         query: `
-          query GetRecentlyItemList($from: Int, $size: Int, $sort: SortBy) {
-            items(from: $from, size: $size, sort: $sort) {
+          query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {
+            axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {    
               results {
-                ...RecentlyItem
-                __typename
+                ...AxieBrief
               }
-              __typename
             }
           }
-          fragment RecentlyItem on LandItem {
-            landType
-            rarity
+        
+          fragment AxieBrief on Axie {
+            id
             name
-            figureURL
-            itemId
-            itemId
-            itemAlias
+            stage
+            class
+            breedCount
+            image
+            title
+            battleInfo {
+              banned
+              __typename
+            }
             auction {
-              ...AxieAuction
+              currentPrice
+              currentPriceUSD
+              __typename
+            }
+            parts {
+              id
+              name
+              class
+              type
+              specialGenes
               __typename
             }
             __typename
           }
-          fragment AxieAuction on Auction {
-            startingPrice
-            endingPrice
-            startingTimestamp
-            endingTimestamp
-            duration
-            timeLeft
-            currentPrice
-            currentPriceUSD
-            suggestedPrice
-            seller
-            listingIndex
-            state
-            __typename
-          }
-        `
+          `
       })
     };
-    let response = await fetch(url, options, 3);
+    let response = await fetch(url, options);
     response = await response.json();
-    console.log('response: ', response);
+    console.log('response success!!');
+
     return {
-      statusCode: 200
+      statusCode: 200,
+      body: JSON.stringify(response.data.axies)
     };
   } catch (error) {
     return {
